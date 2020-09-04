1a74dcbd70257437e8c7534552caf2dca030a926
Ravindranath K <ravindranath.k@espn.com>
2020-09-04 16:03:43 +0530

    wip

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -6,8 +6,7 @@ function ToggleButton (props) {
     const mode = props.mode || 'light',
         onClickHandle = props.onClickHandle,
         className = `${mode} ${props.className}`,
-        switchClassName = `switch`
-        ;
+        switchClassName = `switch`;
 
     return (
         <div className={className} onClick={onClickHandle}>
